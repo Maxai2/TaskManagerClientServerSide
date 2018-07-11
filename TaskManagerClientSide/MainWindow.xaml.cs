dda2df7dffaab559d2098815ba806f9559dfa304
Хамелеон <maxali946@gmail.com>
2018-07-11 12:08:32 +0400

    50%

--- TaskManagerClientSide/MainWindow.xaml.cs
+++ TaskManagerClientSide/MainWindow.xaml.cs
@@ -1,28 +1,104 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.CompilerServices;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
+//----------------------------------------------------------------------
 
 namespace TaskManagerClientSide
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
+        public ObservableCollection<string> ProcessList { get; set; }
+
+        Socket socket;
+        EndPoint ep;
+
+        //var answer = new byte[8192];
+
+        private string conDisConIp = "";
+        public string ConDisConIp
+        {
+            get { return conDisConIp; }
+            set { conDisConIp = value; OnChanged(); }
+        }
+
+        private string runTask;
+        public string RunTask
+        {
+            get { return runTask; }
+            set { runTask = value; OnChanged(); }
+        }
+
+        string ClientString;
+
+        //----------------------------------------------------------------------
+
         public MainWindow()
         {
             InitializeComponent();
+
+            DataContext = this;
+
+            ProcessList = new ObservableCollection<string>();
         }
+
+        //----------------------------------------------------------------------
+
+        private ICommand connectCom;
+        public ICommand ConnectCom
+        {
+            get
+            {
+                if (connectCom is null)
+                {
+                    connectCom = new RelayCommand(
+                        (param) =>
+                        {
+                            //ClientString = string.Empty;
+                            //ClientString = "Connect: " + ConDisConIp;
+
+                            socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                            ep = new IPEndPoint(IPAddress.Parse(ConDisConIp), 7534);
+
+                            socket.Connect(ep);
+
+                            //var data = Encoding.Default.GetBytes(ClientString);
+                            var data = Encoding.Default.GetBytes("Connected");
+                            socket.Send(data);
+                        },
+                        (param) =>
+                        {
+                            if (ConDisConIp != "")
+                            {
+                                return true;
+                            }
+                            else
+                            {
+                                return false;
+                            }
+                        });
+                }
+
+                return connectCom;
+            }
+        }
+
+        //----------------------------------------------------------------------
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnChanged([CallerMemberName]string name = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        //----------------------------------------------------------------------
+
     }
 }
+//----------------------------------------------------------------------
--- TaskManagerServerSide/Program.cs
+++ TaskManagerServerSide/Program.cs
@@ -1,15 +1,93 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace TaskManagerServerSide
 {
     class Program
     {
+        static List<string> ProcessList = new List<string>();
+        static Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+        //--------------------------------------------------------------------
+
         static void Main(string[] args)
         {
+            var ep = new IPEndPoint(IPAddress.Parse("10.2.14.3"), 7534);
+
+            socket.Bind(ep);
+            socket.Listen(10);
+
+            var bytes = new byte[8192];
+
+            var client = socket.Accept();
+
+            while (true)
+            {
+                var length = client.Receive(bytes);
+
+                var msg = Encoding.Default.GetString(bytes, 0, length);
+
+                //Console.WriteLine(msg);
+
+                if (msg != "")
+                {
+                    ParseMsg(msg);
+                    msg = string.Empty;
+                }
+            }
         }
+
+        //--------------------------------------------------------------------
+
+        static void ParseMsg(string msg)
+        {
+            var mode = msg.Substring(0, msg.IndexOf(':'));
+
+            switch (mode)
+            {
+                case "Connect":
+                    {
+                        ProcessList.Clear();
+                        foreach (var item in Process.GetProcesses().OrderBy(f => f.ProcessName))
+                        {
+                            try
+                            {
+                                ProcessList.Add(item.ProcessName);
+                            }
+                            catch (Exception)
+                            { }
+                        }
+
+                        /*var binFormatter = new BinaryFormatter();
+var mStream = new MemoryStream();
+binFormatter.Serialize(mStream, myObjToSerialize);
+
+//This gives you the byte array.
+mStream.ToArray();*/
+
+                        /*var mStream = new MemoryStream();
+var binFormatter = new BinaryFormatter();
+
+// Where 'objectBytes' is your byte array.
+mStream.Write (objectBytes, 0, objectBytes.Length);
+mStream.Position = 0;
+
+var myObject = binFormatter.Deserialize(mStream) as YourObjectType;*/
+                    }
+                    break;
+                case "Run":
+                    break;
+                case "Kill":
+                    break;
+            }
+        }
+
+        //--------------------------------------------------------------------
+
     }
 }
